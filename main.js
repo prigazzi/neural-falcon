a639dddc5a7d211d61022c3cbd422f7e7453c494
Pablo Rigazzi <pablo@Shopify-M1.local>
2022-06-24 00:17:43 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,17 @@
+const canvas = document.querySelector("#myCanvas");
+canvas.width = 200;
+
+const ctx = canvas.getContext('2d');
+const car = new Car(100, 100, 30, 50);
+car.draw(ctx);
+
+animate();
+
+function animate() {
+    canvas.height = window.innerHeight;
+
+    car.update();
+    car.draw(ctx);
+
+    requestAnimationFrame(animate);
+}

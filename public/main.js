04ccf1f88f4e49295acdd2f12178edc982c9f750
Pablo Rigazzi <pablo@Shopify-M1.local>
2022-06-26 16:14:39 +0200

    Adds roads logic with laneCount and current Lane center

--- public/main.js
+++ public/main.js
@@ -2,7 +2,8 @@ const canvas = document.querySelector("#myCanvas");
 canvas.width = 200;
 
 const ctx = canvas.getContext('2d');
-const car = new Car(100, 100, 30, 50);
+const road = new Road(canvas.width / 2, canvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 100, 30, 50);
 car.draw(ctx);
 
 animate();
@@ -10,6 +11,7 @@ animate();
 function animate() {
     canvas.height = window.innerHeight;
 
+    road.draw(ctx);
     car.update();
     car.draw(ctx);
 
